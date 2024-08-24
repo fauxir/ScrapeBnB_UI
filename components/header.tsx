403267c6bd0233a8f7b0935c4467ccacd6042c83
fauxir <fauxir@gmail.com>
2024-08-24 18:09:33 +0100

    Header text change

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ export const Header = () => (
   <div className="w-full flex justify-center align-middle mb-4">
     <div className="flex justify-center align-middle w-full md:w-[80%] opacity-90">
       <SwapTextCard
-        initialText="What if AirBnb had an unofficial free api?"
+        initialText="What if AirBnb had an unofficial free API?"
         finalText="ScrapeBnB is an open-source NPM package that collects AirBnB property data. On this site, you can trial the package and for the full data set review, you can use the API."
       />
     </div>
