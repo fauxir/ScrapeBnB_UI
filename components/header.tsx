862163a6d77d0fe0a6d19d1cc1f0754abf650ea5
fauxir <fauxir@gmail.com>
2024-08-25 06:36:15 +0100

    Form error handdling

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ export const Header = () => (
     <div className="flex justify-center align-middle w-full md:w-[80%]">
       <SwapTextCard
         initialText="What if AirBnB had an unofficial free API?"
-        finalText="ScrapeBnB is an open-source NPM package that collects AirBnB property data. On this site, you can trial the package and for the full data set review, you can use the API."
+        finalText="ScrapeBnB is an open-source NPM package that scrapes AirBnB property data. On this site, you can trial the package and if you want to test the the full data set, you can use the API."
       />
     </div>
   </div>
--- components/property-form.tsx
+++ components/property-form.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from "react";
+import React, { FormEvent, useState, useEffect } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { CheckCircle2, CircleDashed } from "lucide-react";
 import { cn } from "@/utils/cn";
@@ -10,19 +10,45 @@ interface PropertyFormProps {
 
 export const PropertyForm: React.FC<PropertyFormProps> = ({ onSubmit, status }) => {
   const [propertyLink, setPropertyLink] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
   const isEnabled = status === "Submit";
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    
+    // Regex to match the Airbnb URL pattern
+    const urlPattern = /^https:\/\/www\.airbnb\.co\.uk\/rooms\/\d+$/;
+
+    // Check if the link is empty or does not match the required pattern
+    if (!propertyLink || !urlPattern.test(propertyLink)) {
+      setError("Please enter a valid Airbnb link.");
+      setPropertyLink("")
+      return;
+    }
+
+    setError(null);
+    
     if (!isEnabled) return;
+    
     await onSubmit(propertyLink);
     setPropertyLink("");
   };
 
+  // Fade out error message after 3 seconds
+  useEffect(() => {
+    if (error) {
+      const timer = setTimeout(() => {
+        setError(null);
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [error]);
+
   return (
     <div className="w-full lg:w-1/2 lg:max-w-xl">
       <div className="group flex h-fit w-full flex-col justify-between rounded-3xl p-4 md:p-6 bg-white/10 dark:bg-black/10 backdrop-blur-md border border-white/20 dark:border-black/20 shadow-lg opacity-90">
-        <form className="gap-4 h-28 z-50 flex flex-col justify-between" onSubmit={handleSubmit}>
+        <form className="gap-4 h-36 md:h-28 -z-10 flex flex-col justify-between" onSubmit={handleSubmit}>
           <input
             type="text"
             name="airbnb_links"
@@ -35,7 +61,7 @@ export const PropertyForm: React.FC<PropertyFormProps> = ({ onSubmit, status }) 
           <button
             type="submit"
             disabled={!isEnabled}
-            className="group relative h-10 w-full md:max-w-40 overflow-hidden rounded-md bg-[#a7adb2] px-6 text-sm font-semibold text-white transition-colors duration-300 hover:bg-[#767b8d]"
+            className="group -z-10 relative h-10 w-full md:max-w-40 overflow-hidden rounded-md bg-orange-500 px-6 text-sm font-semibold text-white transition-colors duration-300 hover:bg-orange-700"
           >
             <AnimatePresence mode="wait" initial={false}>
               <motion.span
@@ -65,6 +91,19 @@ export const PropertyForm: React.FC<PropertyFormProps> = ({ onSubmit, status }) 
             </AnimatePresence>
           </button>
         </form>
+        <AnimatePresence>
+          {error && (
+            <motion.div
+              className="fixed w-fit z-80 bottom-[67px] md:right-6 md:bottom-6  bg-red-500 text-white py-2 px-1 rounded-md shadow-md"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 20 }}
+              transition={{ duration: 0.3 }}
+            >
+              {error}
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </div>
   );
